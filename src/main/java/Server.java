2117114b32d5fef204ae3921d52735cfad4dc7df
vokor <volodyk.99@mail.ru>
2020-05-23 23:00:35 +0300

    create project, add proto

---
+++ src/main/java/Server.java
@@ -0,0 +1,16 @@
+import io.grpc.Grpc;
+import io.grpc.ManagedChannel;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
+import io.grpc.stub.StreamObserver;
+import com.google.protobuf.MessageLite;
+
+public class Server extends GreeterGrpc.GreeterImplBase {
+
+    @Override
+    public void sayHello(Chat.HelloRequest req, StreamObserver<Chat.HelloReply> responseObserver) {
+        Chat.HelloReply reply = Chat.HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
