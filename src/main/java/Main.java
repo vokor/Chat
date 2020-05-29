cf25410d66aa303838494e7d0777e2f96de29c91
FilippChubukov <filipp.chubukov@mail.ru>
2020-05-29 12:07:51 +0300

    Main : codestyle fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 
 class Main {
-    // Example: Igor 4000
     public static void main(String[] args) {
         Logger logger = Logger.getLogger("Main");
         Server server;
@@ -10,17 +9,17 @@ class Main {
         int port;
         String address = "127.0.0.1";
 
-	System.out.println("Enter SRV or CLI");
-	Scanner in = new Scanner(System.in);
+        System.out.println("Enter SRV or CLI");
+        Scanner in = new Scanner(System.in);
         String text = in.nextLine();
 
-	System.out.println("Enter your name");
-	in = new Scanner(System.in);
+        System.out.println("Enter your name");
+        in = new Scanner(System.in);
         name = in.nextLine();
 
 
-	System.out.println("Enter port");
-	in = new Scanner(System.in);
+        System.out.println("Enter port");
+        in = new Scanner(System.in);
         port = Integer.valueOf(in.nextLine());
 
 	if (text.equals("SRV")) {
@@ -29,8 +28,8 @@ class Main {
                 logger.info("Server");
             } else {
                 System.out.println("Enter server adress");
-		in = new Scanner(System.in);
-        	address = in.nextLine();
+                in = new Scanner(System.in);
+                address = in.nextLine();
                 logger.info("Client");
             }
 
